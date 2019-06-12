d611bafc0c590c6ec5ef399181b7047a11cabb0a
vukal2k <vuanlbv@gmail.com>
2019-06-12 22:08:24 +0700

    create api get don dat hang by nha hang

---
+++ getDonDatHangByNhaHang.php
@@ -0,0 +1,46 @@
+<?php
+    require "database.php";
+
+    $viewModelJson = $_POST["viewModel"];
+    if(isset($viewModelJson)){
+        $viewModel = json_decode($viewModelJson);
+
+        $query = "  SELECT t.idtaikhoan,t.tenkhachhang, d.gioden,d.ngayden,t.sdt
+                    FROM datban d
+                    INNER JOIN nhahang n
+                    ON d.idnhahang = n.idnhahang
+                    INNER JOIN taikhoan t
+                    ON d.idtaikhoan=t.idtaikhoan
+                    WHERE n.idnhahang = ".$viewModel->idNhaHang;
+
+        $filter="";
+        if($viewModel->ngayDenFilterFrom!="" && $viewModel->ngayDenFilterTo!=""){
+            $filter = $filter."AND d.ngayden>='".$viewModel->ngayDenFilterFrom."'
+                                AND d.ngayden <='".$viewModel->ngayDenFilterTo."'";
+        }
+        if($viewModel->searchKey != ""){
+            $filter = $filter."AND t.tenkhachhang LIKE '%".$viewModel->searchKey."%' 
+                                OR t.sdt LIKE '%".$viewModel->searchKey."%' 
+                                OR t.email LIKE '%".$viewModel->searchKey."%'";
+        }
+
+        $query=$query.$filter;
+
+        $database = new database();
+        $result = $database->fetchsql($query);
+
+        echo json_encode($result);
+    }
+?>
+
+<!-- method post, pram: 
+viewModel: {
+		"idNhaHang":"1",
+		"ngayDenFilterFrom": "",
+		"ngayDenFilterTo":"",
+		"searchKey":""
+	}
+
+class ketqua{
+	idtaikhoan,tenkhachhang, gioden,ngayden,sdt
+} -->
