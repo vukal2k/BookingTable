fe13be113ba4811011cdd32a8aad4d8da9cba3dd
vukal2k <vuanlbv@gmail.com>
2019-06-30 00:17:53 +0700

    asd

--- getNhaHangById.php
+++ getNhaHangById.php
@@ -1,39 +1,18 @@
 
 <?php
-  require "dbCon.php";
+  require "database.php";
 
   $idNhaHang = $_GET['idnhahang']; 
+  if(isset($idNhaHang)){
+	$query = "SELECT idnhahang,tennhahang,diachi,sdt,uudai,khoangtien,giodonkhach,hinhanh,mota FROM nhahang WHERE idnhahang = ".$idNhaHang; 
 
-  $query = "SELECT * FROM nhahang WHERE idnhahang = ".$idNhaHang; 
-  $data = mysqli_query($connect, $query);
-
- 
-
-  // 1. Tạo class NhaHang
-class NhaHang{
-	function NhaHang($idnhahang, $idtaikhoan, $tennhahang, $diachi,  $uudai, $loaihinh, $idkhuvuc,  $hinhanh){
-		$this->IdNhaHang = $idnhahang;
-		$this->IdTaiKhoan=$idtaikhoan;
-		$this->TenNhaHang = $tennhahang;
-		$this->DiaChi = $diachi;
-		$this->UuDai = $uudai;
-		$this->LoaiHinh = $loaihinh;
-		$this->IdKhuVuc = $idkhuvuc;
-		$this->HinhAnh = $hinhanh;
-		
-	}
+	$database = new database();
+	$result = $database->fetchsql($query);
+	
+	echo json_encode($result[0]);
 }
-// 2. Tạo mảng
-$mangNH = array();
-//3. Thêm phần tử vào mảng
-
-while ($row = mysqli_fetch_assoc($data)) {
-  	array_push($mangNH, new NhaHang($row ['idnhahang'],  $row ['idtaikhoan'], $row ['tennhahang'], $row ['diachi'], $row ['uudai'], $row ['loaihinh'], $row ['idkhuvuc'], $row ['hinhanh']));
-
-  }
-
-//4. Chuyển định dạng của mảng -> Json
-echo json_encode($mangNH[0]);
+  
+  
 ?>
 
 <!-- http://localhost/server/getNhaHangById.php?idnhahang=1 -->
